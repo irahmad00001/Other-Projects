e7d1dbd3e271c85998b5796b3525ba18c90bdef3
Ahmad Azizi <IRAhamd00001@gmail.com>
2022-11-21 15:36:36 +0330

    Update N-Queen With Backtracking.

--- N-Queens With Backtracking/src/Main.java
+++ N-Queens With Backtracking/src/Main.java
@@ -1,33 +1,6 @@
-import java.util.ArrayList;
-
 public class Main {
-	public static boolean check(MyStack<Integer> input) {
-		Object[] arr = input.copyTo().toArray();
-		for(int i = 0 ; i < arr.length ; i++) {
-			for(int j = (i + 1) ; j < (arr.length - 1) ; i++) {
-				if(arr[i] == arr[j]) {
-					return false;
-				}
-				if(Math.abs((int)arr[i] - (int)arr[j]) == Math.abs(i - j)) {
-					return false;
-				}
-			}
-	}
-		return true;
-	}
 	public static void main(String[] args) {
-		MyStack<Integer> stack1 = new MyStack<Integer>(4);
-		stack1.push(2);
-		stack1.push(0);
-		stack1.push(3);
-		stack1.push(1);
-		System.out.println(check(stack1));
-		
-		for(int i = 0 ; i < 8; i++) {
-			stack.push(i);
-			for(int j = 0 ; j < 8 ; j++) {
-				if(check()
-			}
-		}
-		}
+		N_Queen nq = new N_Queen(4);
+		nq.show_Answer();
+	}
 }
--- N-Queens With Backtracking/src/N_Queen.java
+++ N-Queens With Backtracking/src/N_Queen.java
@@ -1,56 +1,56 @@
+import java.util.Arrays;
+
 public class N_Queen {
-	private static int number;
+	
+	private int number;
 	public N_Queen(int number) {
 		this.number = number;
 	}
 	private boolean[][] arr;
-	public void zero() {
+	
+	public void show_Answer() {
 		arr = new boolean[number][number];
 		for(int i = 0 ; i < number ; i++) {
-			for(int j = 0 ; j < number ; j++)
-			arr[i][j] = false;
+			for(int j = 0 ; j < number ; j++) {
+				arr[i][j] = false;
+			}
 		}
-	}
-	public boolean[][] getArr() {
-		return arr;
-	}
-	public void setArr(boolean[][] arr) {
-		this.arr = arr;
+		backTracking(0);
+		System.out.println(Arrays.deepToString(arr));
 	}
 	
-	public boolean backTracking(int  _j) {
-		if(_j >= number) {
+	private boolean backTracking (int _j) {
+		if(_j >= number){
 			return true;
 		}
-		for(int i = 0; i < number ; i++) {
-			if(isSafe(arr,i,_j)) {
+		for(int i = 0 ; i < number ; i++) {
+			if(isSafe(arr,i ,_j)) {
 				arr[i][_j] = true;
-				if(backTracking(_j + 1))
+				if(backTracking(_j+1)) {
 					return true;
+				}
 				arr[i][_j] = false;
 			}
 		}
-		return false;
+		return false;		
 	}
 	
-	
-	public static boolean isSafe(boolean board[][] , int _i , int _j) {
-		
+	private boolean isSafe(boolean[][] arr , int _i , int _j) {
 		for(int i = 0 ; i < number ; i++) {
-			if(board[_i][i] == true) {
+			if(arr[_i][i] == true) {
 				return false;
 			}
 		}
 		
 		for(int j = 0 ; j < number ; j++) {
-			if(board[j][_j] == true) {
+			if(arr[j][_j] == true) {
 				return false;
 			}
 		}
 		
 		for(int i = 0 ; i < number ; i++) {
 			for(int j = 0 ; j < number ; j++) {
-					if(board[i][j] == true) {{
+					if(arr[i][j] == true) {{
 						if(i - _i == j - _j) {
 						return false;
 						}
